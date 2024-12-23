e72bf226cb76adcfdfbbba40e2cd814a451774d4
JureRelja <jurereljanovic99@gmail.com>
2024-12-23 12:36:42 +0100

    switched the template

--- next.config.js
+++ next.config.js
@@ -1,27 +1,9 @@
-import { withPayload } from '@payloadcms/next/withPayload'
+/**
+ * @type {import('next').NextConfig}
+ */
 
-import redirects from './redirects.js'
-
-const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
-  ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-  : undefined || process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
-
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
-        const url = new URL(item)
-
-        return {
-          hostname: url.hostname,
-          protocol: url.protocol.replace(':', ''),
-        }
-      }),
-    ],
-  },
   reactStrictMode: true,
-  redirects,
-}
+};
 
-export default withPayload(nextConfig)
+module.exports = nextConfig;
